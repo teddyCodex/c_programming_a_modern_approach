7a2ee85c580ab68cde941a42ceec8d50fbcfc85b
ubunty6ty9 <samted.uche@gmail.com>
2022-10-21 23:20:30 +0100

    skeleton of project 8

---
+++ chapter2_exercises/8-loanCalc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int loanCalc(float loan, float rate, float repayAmount)
+{
+  printf("Loan Repayment Calculator");
+  printf("\n*********************\n");
+
+  return (0);
+}
+
+int main()
+{
+  loanCalc(20000, 6.0, 386.66);
+}
