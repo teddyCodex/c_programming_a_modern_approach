16e99226d9e24ba33b3f9437ae1318230f013012
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-10-22 01:49:36 +0100

    tweaked program to calculate loan balance until fully repaid

--- chapter2_exercises/8-loanCalc.c
+++ chapter2_exercises/8-loanCalc.c
@@ -1,42 +1,50 @@
+// Program that calculates loan repayment till 0 given several pieces of information.
+// Assumption - all user input will be positive integers
+
 #include <stdio.h>
+#define MONTHS_IN_YEAR 12
 
 // function to calculate the monthly interest on the remaining loan amount
-float monthly_int(float rate, float tenure)
+float monthly_int(float rate, float months_in_year)
 {
-  float x = ((rate / 100.0) / tenure);
-  return (x);
+  float x = ((rate / 100.0) / months_in_year);
+  return x;
 }
 
 // function to calculate the loan balance after removing repayment and adding interest
 float monthly_bal(float loan, float monthlyRate, float repay_amount)
 {
-  float x = (loan * (1 + monthlyRate)) - repay_amount;
-  return x;
+  float i = (loan * (1 + monthlyRate)) - repay_amount;
+  return i;
 }
 
-// function to calculate and print loan balance for 12 months.
-int loanCalc(float loan, float rate, float repay_amount, float tenure)
+// function to calculate and print loan balance till loan is fully repaid.
+int loanCalc(float loan, float rate, float repay_amount, float months_in_year)
 {
   printf("Loan Repayment Calculator\n*************************\n");
 
-  float monthlyRate = monthly_int(rate, tenure);
+  float monthlyRate = monthly_int(rate, months_in_year);
 
   int startMonth = 1;
 
-  while (startMonth <= tenure)
+  while (loan > repay_amount)
   {
     loan = monthly_bal(loan, monthlyRate, repay_amount);
-    printf("Month %02d balance - S%.2f\n", startMonth, loan);
+    printf("Month %02d balance = $%.2f\n", startMonth, loan);
     startMonth++;
+    if (loan <= repay_amount)
+    {
+      printf("Month %02d: Congrats! Loan is fully repaid!\n", startMonth);
+    }
   }
 
-  return (0);
+  return 0;
 }
 
 // main function to receive user input on loan info
 int main()
 {
-  float loan, rate, repay_amount, tenure;
+  float loan, rate, repay_amount;
 
   printf("Enter loan amount: ");
   scanf("%f", &loan);
@@ -44,8 +52,6 @@ int main()
   scanf("%f", &rate);
   printf("Enter repayment amount: ");
   scanf("%f", &repay_amount);
-  printf("Enter the tenure(in months): ");
-  scanf("%f", &tenure);
 
-  loanCalc(loan, rate, repay_amount, tenure);
+  loanCalc(loan, rate, repay_amount, MONTHS_IN_YEAR);
 }
