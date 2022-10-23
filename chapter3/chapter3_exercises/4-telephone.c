1d081cfe17ab4f1defd16a5fbfb3f422a61a8c18
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-10-23 01:06:23 +0100

    program that formats telephone number based on user input

---
+++ chapter3/chapter3_exercises/4-telephone.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b, c;
+
+    printf("Enter phone number [(xxx) xxx-xxxx]: ");
+    scanf("( %d) %d- %d", &a, &b, &c);
+
+    printf("You entered %d.%d.%d\n", a, b, c);
+
+    return 0;
+}
