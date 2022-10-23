1263b31c9b95854e4d1b915ad08e0cf1271c7ee4
ubunty6ty9 <samted.uche@gmail.com>
2022-10-23 14:23:48 +0100

    program that computes the upc check digit

---
+++ chapter4/1-upcCheck.c
@@ -0,0 +1,24 @@
+/* Program that computes a Universal Product Code check digit */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, check, sum1, sum2, total;
+
+	printf("Enter the first digit: ");
+	scanf("%1d", &d1);
+	printf("Enter the first group of 5 digits: ");
+	scanf("%1d%1d%1d%1d%1d", &d2, &d3, &d4, &d5, &d6);
+	printf("Enter the second group of 5 digits: ");
+	scanf("%1d%1d%1d%1d%1d", &d7, &d8, &d9, &d10, &d11);
+
+	sum1 = d1 + d3 + d5 + d7 +d9 + d11;
+	sum2 = d2 + d4 + d6 + d8 + d10;
+	total = 3 * sum1 + sum2;
+	check = 9 - ((total - 1) % 10);
+
+	printf("Check Digit is %1d\n", check );
+
+	return 0;
+}
