55fa303c50cc78c973d46a719db0051599d36ccb
Ubuntu6ty9 <15376476+ubuntu6ty9@users.noreply.github.com>
2022-11-01 08:58:23 +0100

    project 5

--- chapter4/chapter4_projects/5-upcCheck.c
+++ chapter4/chapter4_projects/5-upcCheck.c
@@ -4,22 +4,47 @@
 
 int main(void)
 {
-	long upc = 6543298765;
+	long upc;
 	int check, sum1, sum2, total;
 	int d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11;
 
-	// printf("Enter the first 11 digits of a UPC: ");
-	// scanf("%ld", &upc);
+	printf("Enter the first 11 digits of a UPC: ");
+	scanf("%ld", &upc);
 
+	// This program will be refactored in good time lol
+	d11 = upc % 10;
+	upc /= 10;
+	d10 = upc % 10;
+	upc /= 10;
+	d9 = upc % 10;
+	upc /= 10;
+	d8 = upc % 10;
+	upc /= 10;
+	d7 = upc % 10;
+	upc /= 10;
+	d6 = upc % 10;
+	upc /= 10;
+	d5 = upc % 10;
+	upc /= 10;
+	d4 = upc % 10;
+	upc /= 10;
+	d3 = upc % 10;
+	upc /= 10;
+	d2 = upc % 10;
 	d1 = upc / 10;
-	printf("%d\n", d1);
 
-	// sum1 = d1 + d3 + d5 + d7 + d9 + d11;
-	// sum2 = d2 + d4 + d6 + d8 + d10;
-	// total = 3 * sum1 + sum2;
-	// check = 9 - ((total - 1) % 10);
+	// printf("%d\n", d11);
+	// printf("%d %d %d %d %d %d %d %d %d %d %d\n", d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11);
 
-	// printf("Check Digit is %d\n", upc);
+	sum1 = d1 + d3 + d5 + d7 + d9 + d11;
+	sum2 = d2 + d4 + d6 + d8 + d10;
+	total = 3 * sum1 + sum2;
+	check = 9 - ((total - 1) % 10);
+
+	// printf("sum 1 = %d\n", sum1);
+	// printf("sum 2 = %d\n", sum2);
+	// printf("total = %d\n", total);
+	printf("Check Digit is %d\n", check);
 
 	return 0;
 }
